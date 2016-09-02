bf41aef94879971e42a629779a8fb2abf9939f03
Ivan Li <ivan.li@invigorgroup.com>
2016-09-02 05:38:43 +0000

    add event to profile management

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Events\User\Profile\ProfileEditViewed;
+use App\Events\User\Profile\ProfileUpdated;
+use App\Events\User\Profile\ProfileUpdating;
+use App\Events\User\Profile\ProfileViewed;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -18,21 +22,25 @@ class ProfileController extends Controller
 
     /**
      * Edit page of my profile
+     * @param Request $request
      * @return $this
      */
     public function index(Request $request)
     {
         $user = auth()->user();
+        event(new ProfileEditViewed($user));
         return view('user.profile.edit')->with(compact(['user']));
     }
 
     /**
      * Detail page of other users' profile
      * @param $id
+     * @return $this
      */
     public function show($id)
     {
         $user = User::findOrFail($id);
+        event(new ProfileViewed($user));
         return view('user.profile.index')->with(compact(['user']));
     }
 
@@ -45,11 +53,11 @@ class ProfileController extends Controller
     public function update(Request $request, $id)
     {
         /*TODO validation here*/
-
-
         $user = User::findOrFail($id);
+        event(new ProfileUpdating($user));
         $user->update($request->all());
         $status = true;
+        event(new ProfileUpdated($user));
         return redirect()->route("profile.index");
     }
 }
