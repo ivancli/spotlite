8e41cbe5ce34c19427aa66aba58d69234bf0903b
vagrant <vagrant@homestead>
2016-09-29 17:45:01 +1000

    update db

---
+++ database/migrations/2016_09_29_170448_alter_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('reports', function ($table) {
+            $table->integer('user_id');
+            $table->foreign('user_id')->references('user_id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('reports', function (Blueprint $table) {
+            $table->dropForeign('reports_user_id_foreign');
+        });
+    }
+}
---
+++ resources/views/report/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ivan.li
+ * Date: 9/29/2016
+ * Time: 4:53 PM
+ */
