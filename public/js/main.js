ec1a5bbbd6be094ef591912dd755c9d5d0c044c3
vagrant <vagrant@homestead>
2016-10-11 10:16:55 +1100

    SPOT-128

--- public/js/main.js
+++ public/js/main.js
@@ -35625,8 +35625,6 @@ $(function () {
 
     var today = timestampToDateTimeByFormat(new Date().getTime() / 1000, 'Y-m-d');
 
-
-
     if (typeof user != 'undefined' && typeof user.is_first_login != 'undefined' && user.subscription != null) {
         /* clean up unused localStorage */
         $.each(localStorage, function (key, value) {
@@ -35657,6 +35655,11 @@ $(function () {
                 });
                 localStorage.setItem("met-first-login-welcome-msg-" + today + "-" + user.user_id, 1);
             });
+        } else {
+            if (typeof cc_expire_within_a_month != 'undefined' && cc_expire_within_a_month == true && localStorage.getItem("met-cc-expiry-msg-" + today + "-" + user.user_id) != 1) {
+                localStorage.setItem("met-cc-expiry-msg-" + today + "-" + user.user_id, 1);
+                showCreditCardExpiry();
+            }
         }
     } else {
         if (typeof cc_expire_within_a_month != 'undefined' && cc_expire_within_a_month == true && localStorage.getItem("met-cc-expiry-msg-" + today + "-" + user.user_id) != 1) {
