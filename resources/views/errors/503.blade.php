b536770e9d0ad643e56d532cd52744cca73a0c7b
vagrant <vagrant@homestead>
2016-11-08 05:46:25 +0000

    update 503

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,54 @@
-@extends('layouts.adminlte_no_sidebar')
-@section('title', 'Be right back')
-@section('content')
-    <h4>The site is under maintenance, will be right back ASAP.</h4>
-@stop
+<!doctype html>
+<title>SpotLite Maintenance</title>
+<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+<link rel="shortcut icon" type="image/x-icon" href="{{asset('images/favicon.ico')}}"/>
+{{--<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">--}}
+<style>
+    body, h1, h2, h3, a, .main-header .header-label, div{
+        font-family: 'Lato', sans-serif;
+    }
+</style>
+<style>
+    body {
+        text-align: center;
+        padding: 150px;
+    }
+
+    h1 {
+        font-size: 50px;
+    }
+
+    body {
+        font-size: 20px;
+        color: #333;
+    }
+
+    article {
+        display: block;
+        text-align: left;
+        width: 650px;
+        margin: 0 auto;
+    }
+
+    a {
+        color: #dc8100;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #333;
+        text-decoration: none;
+    }
+</style>
+
+<article>
+    <h1>We&rsquo;ll be back soon!</h1>
+    <div>
+        <p>
+            Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you
+            can always <a href="mailto:admin@spotlite.com.au">contact us</a>, otherwise we&rsquo;ll be back online
+            shortly!
+        </p>
+        <p>&mdash; SpotLite Admin</p>
+    </div>
+</article>
