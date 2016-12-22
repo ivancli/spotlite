4954b569b908b35cdecf9a526710299012685f32
vagrant <vagrant@homestead>
2016-12-22 02:37:44 +0000

    added survey

--- resources/views/msg/subscription/raw/cancelled.blade.php
+++ resources/views/msg/subscription/raw/cancelled.blade.php
@@ -4,6 +4,8 @@
             <div class="box-body">
                 <h4>Subscription has been cancelled</h4>
                 <p>You have cancelled the subscription of <strong>{{$apiSubscription->product()->name}}</strong>.</p>
+                <p>We would love to know what made you want to cancel. Please <a
+                            href="https://www.surveymonkey.com/r/7QCRNHY" target="_blank">send us your feedback</a>!</p>
             </div>
         </div>
     </div>
