e2246a6f4660dc15125795ad4733afb6d66d356b
vagrant <vagrant@homestead>
2017-01-04 04:56:36 +0000

    added missing faq link

--- resources/views/products/alert/notification_popup.blade.php
+++ resources/views/products/alert/notification_popup.blade.php
@@ -66,7 +66,7 @@
             <div class="modal-body">
                 <div class="warnning-message-container text-danger m-b-10" style="display: none;">
                     <i class="fa fa-info-circle"></i> &nbsp; For this alert to be set up, you need to
-                    <a href="#" class="text-danger" style="text-decoration: underline">nominate your site URL</a>
+                    <a href="https://spotlitehelp.zendesk.com/hc/en-us/articles/235847887-How-do-I-nominate-My-Site-URL-" class="text-danger" style="text-decoration: underline">nominate your site URL</a>
                 </div>
 
                 <div class="basic-notifications m-b-20"
