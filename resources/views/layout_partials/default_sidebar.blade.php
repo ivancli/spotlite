8819baade43ae9ab5c3fe4fea5c295324fe124cf
vagrant <vagrant@homestead>
2017-01-16 05:00:57 +0000

    apply permission check for the whole sidebar

--- resources/views/layout_partials/default_sidebar.blade.php
+++ resources/views/layout_partials/default_sidebar.blade.php
@@ -4,69 +4,79 @@
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu">
             @if(auth()->check() && (!auth()->user()->needSubscription || (!is_null(auth()->user()->subscription) && auth()->user()->subscription->isValid())))
-                <li class="treeview {{Style::set_active('/')}} {{Style::set_active_starts_with('dashboard')}}">
-                    <a href="#">
-                        <i class="fa fa-dashboard"></i>
-                        <span>DASHBOARDS</span>
-                        <span class="pull-right-container">
+                @if(auth()->user()->can(['manage_dashboard', 'manage_dashboard_widget']))
+                    <li class="treeview {{Style::set_active('/')}} {{Style::set_active_starts_with('dashboard')}}">
+                        <a href="#">
+                            <i class="fa fa-dashboard"></i>
+                            <span>DASHBOARDS</span>
+                            <span class="pull-right-container">
                           <i class="fa fa-caret-down pull-right"></i>
                         </span>
-                    </a>
-                    <ul class="treeview-menu">
-                        @foreach(auth()->user()->nonHiddenDashboards as $index=>$dashboard)
-                            <li class="{{Style::set_active_and(array('dashboard', $dashboard->getKey()))}}">
-                                <a href="{{route('dashboard.show', $dashboard->getKey())}}" class="lnk-dashboard">
-                                    <i class="fa fa-circle-o"></i>
-                                    <span class="lnk-dashboard-{{$dashboard->getKey()}}">
+                        </a>
+                        <ul class="treeview-menu">
+                            @foreach(auth()->user()->nonHiddenDashboards as $index=>$dashboard)
+                                <li class="{{Style::set_active_and(array('dashboard', $dashboard->getKey()))}}">
+                                    <a href="{{route('dashboard.show', $dashboard->getKey())}}" class="lnk-dashboard">
+                                        <i class="fa fa-circle-o"></i>
+                                        <span class="lnk-dashboard-{{$dashboard->getKey()}}">
                                         {{$dashboard->dashboard_name}}
                                     </span>
-                                    @if(auth()->user()->nonHiddenDashboards()->count() > 1)
-                                        @if($index==0)
-                                            <span class="pull-right-container btn-reorder-dashboard"
-                                                  data-order="{{$index}}"
-                                                  data-dashboard-id="{{$dashboard->getKey()}}"
-                                                  onclick="swapDashboard(this); event.preventDefault(); event.stopPropagation(); return false;">
+                                        @if(auth()->user()->nonHiddenDashboards()->count() > 1)
+                                            @if($index==0)
+                                                <span class="pull-right-container btn-reorder-dashboard"
+                                                      data-order="{{$index}}"
+                                                      data-dashboard-id="{{$dashboard->getKey()}}"
+                                                      onclick="swapDashboard(this); event.preventDefault(); event.stopPropagation(); return false;">
                                                 <i class="fa fa-arrow-down"></i>
                                             </span>
-                                        @else
-                                            <span class="pull-right-container btn-reorder-dashboard"
-                                                  data-order="{{$index}}"
-                                                  data-dashboard-id="{{$dashboard->getKey()}}"
-                                                  onclick="swapDashboard(this); event.preventDefault(); event.stopPropagation(); return false;">
+                                            @else
+                                                <span class="pull-right-container btn-reorder-dashboard"
+                                                      data-order="{{$index}}"
+                                                      data-dashboard-id="{{$dashboard->getKey()}}"
+                                                      onclick="swapDashboard(this); event.preventDefault(); event.stopPropagation(); return false;">
                                                 <i class="fa fa-arrow-up"></i>
                                             </span>
+                                            @endif
                                         @endif
-                                    @endif
-                                </a>
-                            </li>
-                        @endforeach
-                        <script type="text/javascript">
-                        </script>
-                    </ul>
-                </li>
-                <li>
-                    <a href="#" onclick="showAddDashboardForm(this); return false;" id="btn-add-new-dashboard">
-                        <i class="fa fa-plus"></i>
-                        <span>ADD A NEW DASHBOARD</span>
-                    </a>
-                </li>
-                <li class="{{Style::set_active_starts_with('product')}} lnk-product">
-                    <a href="{{url('product')}}">
-                        <i class="fa fa-tag"></i>
-                        <span>PRODUCTS</span>
-                    </a>
-                </li>
-                <li class="{{Style::set_active('alert')}} lnk-alert">
-                    <a href="{{url('alert')}}">
-                        <i class="fa fa-bell-o"></i>
-                        <span>ALERTS</span></a>
-                </li>
-                <li class="{{Style::set_active('report')}}">
-                    <a href="{{url('report')}}">
-                        <i class="fa fa-envelope-o"></i>
-                        <span>REPORTS</span>
-                    </a>
-                </li>
+                                    </a>
+                                </li>
+                            @endforeach
+                            <script type="text/javascript">
+                            </script>
+                        </ul>
+                    </li>
+                @endif
+                @if(auth()->user()->can('manage_dashboard'))
+                    <li>
+                        <a href="#" onclick="showAddDashboardForm(this); return false;" id="btn-add-new-dashboard">
+                            <i class="fa fa-plus"></i>
+                            <span>ADD A NEW DASHBOARD</span>
+                        </a>
+                    </li>
+                @endif
+                @if(auth()->user()->can('manage_product'))
+                    <li class="{{Style::set_active_starts_with('product')}} lnk-product">
+                        <a href="{{url('product')}}">
+                            <i class="fa fa-tag"></i>
+                            <span>PRODUCTS</span>
+                        </a>
+                    </li>
+                @endif
+                @if(auth()->user()->can('manage_alert'))
+                    <li class="{{Style::set_active('alert')}} lnk-alert">
+                        <a href="{{url('alert')}}">
+                            <i class="fa fa-bell-o"></i>
+                            <span>ALERTS</span></a>
+                    </li>
+                @endif
+                @if(auth()->user()->can('manage_report'))
+                    <li class="{{Style::set_active('report')}}">
+                        <a href="{{url('report')}}">
+                            <i class="fa fa-envelope-o"></i>
+                            <span>REPORTS</span>
+                        </a>
+                    </li>
+                @endif
             @endif
             @if(auth()->check() && auth()->user()->isStaff)
                 @if(auth()->user()->can('manage_app_preference'))
