df77209e88feddeac5034185a922418a7737ba14
Ivan Li <ivan.li@INVIGOR.local>
2017-04-12 13:04:51 +1000

    updated wording

--- resources/views/user/profile/forms/edit.blade.php
+++ resources/views/user/profile/forms/edit.blade.php
@@ -240,7 +240,7 @@
                                @if(!is_null($user->reportTask) && $user->reportTask->show_all == 'y')
                                checked="checked"
                                @endif
-                               value="y"> Show all Product Page URLs in SpotLite Digest
+                               value="y"> Show all Product Page URLs in SpotLite Digest (even if the Category or Product price hasn't changed)
                     </label>
                 </div>
             </div>
