8239893b6234658050b811f2bedc4fa09e0e144e
Ivan Li <ivan.li@INVIGOR.local>
2017-04-28 11:02:50 +1000

    fix parser issue

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -14,6 +14,8 @@ use App\Models\Crawler;
 use App\Models\Site;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Invigor\Crawler\Repositories\Crawlers;
+use Invigor\Crawler\Repositories\Crawlers\DefaultCrawler;
 use Maatwebsite\Excel\Facades\Excel;
 
 class TestController extends Controller
@@ -29,61 +31,14 @@ class TestController extends Controller
 
     public function index()
     {
-        $url = "http://www.ebay.com.au/itm/Tornado-Bluetooth-Smart-Padlock-/222389229596?&_trksid=p2056016.m2518.l4276";
-        $path = parse_url($url)['path'];
-        $tokens = explode('/', $path);
-        $itemId = $tokens[count($tokens) - 1];
-        if ($itemId) {
-            $item = $this->ebayRepo->getItem($itemId);
-            if (isset($item->errors)) {
-                $itemGroup = $this->ebayRepo->getItemGroup($itemId);
-                if (isset($itemGroup->items) && is_array($itemGroup->items)) {
-                    $itemGroupItem = array_first($itemGroup->items);
-                    $itemId = $itemGroupItem->itemId;
-                    $item = $this->ebayRepo->getItem($itemId);
-                }
-            }
-            $ebayItem = $site->ebayItem;
-            if (is_null($ebayItem)) {
-                $ebayItem = $site->ebayItem()->save(new EbayItem());
-            }
+        $defaultCrawler = app(DefaultCrawler::class);
 
-            $ebayItem->title = isset($item->title) ? $item->title : null;
-            $ebayItem->subtitle = isset($item->subtitle) ? $item->subtitle : null;
-            $ebayItem->shortDescription = isset($item->shortDescription) ? $item->shortDescription : null;
-            $ebayItem->price = isset($item->price) && isset($item->price->value) ? $item->price->value : null;
-            $ebayItem->currency = isset($item->price) && isset($item->price->currency) ? $item->price->currency : null;
-            $ebayItem->category = isset($item->categoryPath) ? $item->categoryPath : null;
-            $ebayItem->condition = isset($item->condition) ? $item->condition : null;
-            $ebayItem->location_city = isset($item->itemLocation) && isset($item->itemLocation->city) ? $item->itemLocation->city : null;
-            $ebayItem->location_postcode = isset($item->itemLocation) && isset($item->itemLocation->postalCode) ? $item->itemLocation->postalCode : null;
-            $ebayItem->location_country = isset($item->itemLocation) && isset($item->itemLocation->country) ? $item->itemLocation->country : null;
-            $ebayItem->image_url = isset($item->image) && isset($item->image->imageUrl) ? $item->image->imageUrl : null;
-            $ebayItem->brand = isset($item->brand) ? $item->brand : null;
-            $ebayItem->seller_username = isset($item->seller) && isset($item->seller->username) ? $item->seller->username : null;
-
-            $ebayItem->save();
-        }
-//        $site = Site::findOrFail(45835);
-//        $product = $site->product;
-//        if (!is_null($product)) {
-//            $companyUrl = $product->user->company_url;
-//            $ebayUsername = $product->user->ebay_username;
-//
-//            $myCompanyDomain = parse_url($companyUrl)['host'];
-//
-//            list($dummy, $subdomainSplitted) = explode('.', $site->domain, 2);
-//            list($dummy, $domainSplitted) = explode('.', $myCompanyDomain, 2);
-//
-//            //matching both sub-domain and domain
-//            $ebayItem = $site->ebayItem;
-//            if (!is_null($ebayUsername) && !empty($ebayUsername) && !is_null($ebayItem)) {
-//                return $ebayUsername == $ebayItem->seller_username ? 'y' : 'n';
-//            } elseif ($subdomainSplitted == $domainSplitted) {
-//                return 'y';
-//            }
-//        }
-//        return 'n';
-
+        $options = array(
+            "url" => "http://musiciansoasis.com.au/ibanez/467-ibanez-m80m-8-string-meshuggah-signature-electric-guitar.html",
+        );
+        $defaultCrawler->setOptions($options);
+        $defaultCrawler->loadHTML();
+        $html = $defaultCrawler->getHTML();
+        dd($html);
     }
 }
