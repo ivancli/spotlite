0bbfc531351d038b81c9b37c388dfcb29a9a7606
Ivan Li <Ivan Li>
2017-05-16 23:36:49 +1000

    temp remove sub checking

--- app/Jobs/CrawlSite.php
+++ app/Jobs/CrawlSite.php
@@ -50,10 +50,10 @@ class CrawlSite extends Job implements ShouldQueue
         if (isset($this->crawler->site) && isset($this->crawler->site->product) && isset($this->crawler->site->product->user)) {
             $user = $this->crawler->site->product->user;
             /*check user subscription status*/
-            if ($user->needSubscription && (is_null($this->crawler->site->product->user->subscription) || !$this->crawler->site->product->user->subscription->isValid())) {
-                $this->crawler->resetStatus();
-                return false;
-            }
+//            if ($user->needSubscription && (is_null($this->crawler->site->product->user->subscription) || !$this->crawler->site->product->user->subscription->isValid())) {
+//                $this->crawler->resetStatus();
+//                return false;
+//            }
         }
 
         $crawler_class = "DefaultCrawler";
