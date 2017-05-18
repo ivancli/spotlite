4da0d4af8a2a003aa143e7e8d1392627c0baf47c
Ivan Li <Ivan Li>
2017-05-18 13:53:42 +1000

    quick fix

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -10,6 +10,7 @@ namespace App\Http\Controllers;
 
 use App\Contracts\Repository\Ebay\EbayContract;
 use App\Jobs\CrawlSite;
+use App\Models\Category;
 use App\Models\Crawler;
 use App\Models\Site;
 use App\Models\SitePreference;
@@ -33,5 +34,27 @@ class TestController extends Controller
 
     public function index()
     {
+        $user_id = $this->request->get('user_id');
+        $categories = Category::where('user_id', '=', $user_id)->get();
+        $data = $categories;
+        $fileName = "full_report";
+        $excel = Excel::create("{$user_id}_{$fileName}", function ($excel) use ($categories, $data, $fileName) {
+            $excel->sheet("full_report", function ($sheet) use ($data) {
+                $sheet->loadview('products.report.excel.user', compact(['data']));
+                $sheet->setColumnFormat(array(
+                    'D' => \PHPExcel_Style_NumberFormat::FORMAT_CURRENCY_USD_SIMPLE,
+                    'E' => \PHPExcel_Style_NumberFormat::FORMAT_DATE_YYYYMMDD2,
+                    'F' => \PHPExcel_Style_NumberFormat::FORMAT_CURRENCY_USD_SIMPLE,
+                    'G' => \PHPExcel_Style_NumberFormat::FORMAT_DATE_YYYYMMDD2,
+                ));
+                $sheet->setWidth('A', 30);
+                $sheet->setWidth('B', 30);
+                $sheet->setWidth('C', 30);
+                $sheet->setWidth('D', 20);
+                $sheet->setWidth('E', 20);
+                $sheet->setWidth('F', 20);
+                $sheet->setWidth('G', 20);
+            });
+        })->store('csv');
     }
 }
