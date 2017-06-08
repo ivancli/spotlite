c3f060bac01566099eeb54d97f5fad0c1432d9b3
Ivan Li <ivan.li@INVIGOR.local>
2017-06-08 11:24:50 +1000

    updated custom command

--- app/Console/Commands/Custom.php
+++ app/Console/Commands/Custom.php
@@ -9,7 +9,55 @@
 namespace App\Console\Commands;
 
 
-class Custom
-{
+use App\Models\Product;
+use App\Models\ProductMeta;
+use App\Models\User;
+use Illuminate\Console\Command;
+use Maatwebsite\Excel\Facades\Excel;
 
+class Custom extends Command
+{
+    protected $signature = "custom";
+    protected $description = 'Pushing available crawlers to queue';
+
+    protected $crawler = null;
+
+    public function handle()
+    {
+
+        $products = [];
+        $result = Excel::load(storage_path('import/musos_product.csv'), function ($reader) use (&$products) {
+            $data = $reader->all();
+            foreach ($data as $index => $product) {
+                $rowNumber = $index + 2;
+
+                $productData = $product->all();
+                $products [] = $productData;
+            }
+        }, 'Windows-1252');
+
+        $user = User::findOrFail(219);
+
+        $this->output->progressStart(count($products));
+
+        foreach ($products as $product) {
+            $category_name = array_get($product, 'category');
+            $product_name = array_get($product, 'product');
+            $sku = array_get($product, 'sku');
+            $supplier = array_get($product, 'supplier');
+            $brand = array_get($product, 'brand');
+            $cost_price = array_get($product, 'cost_price');
+
+            $category = $user->categories()->where('category_name', $category_name)->get();
+            if (!is_null($category)) {
+                $userProduct = $category->products()->where('product_name', $product_name)->get();
+                if (!is_null($userProduct)) {
+                    $meta = $userProduct->meta;
+                    $meta->update($product);
+                }
+            }
+            $this->output->progressAdvance();
+        }
+        $this->output->progressFinish();
+    }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,6 +22,7 @@ class Kernel extends ConsoleKernel
         Commands\FullReport::class,
         Commands\BulkImport::class,
         Commands\Alert::class,
+        Commands\Custom::class,
     ];
 
     protected $crawler;
